c02f27b7eba02d187da5ef89d5cdaeaf6e87d409
Danny Doan <dannykdoan@gmail.com>
2022-09-24 16:30:38 -0700

    animate motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AnimatePresence, motion } from 'framer-motion'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div className='bg-neutral-900 text-white'>
-      <Component {...pageProps} />
+        <AnimatePresence>
+          <motion.div
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          exit={{ y: 10, opacity: 0 }}>
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence> 
     </div>
   )
 }
