4da7ddde5637aeec61eed3524041f3c146476458
Danny Doan <dannykdoan@gmail.com>
2022-09-25 03:03:21 -0700

    changed padding from app.tsx instead of nav bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar'
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    <div className='bg-[#0F0E0E] text-white mx-auto max-w-4xl space-y-8 py-10 px-5 lg:px-0'>
+    <div className='bg-[#0F0E0E] text-white mx-auto max-w-4xl space-y-8 py-6 px-5 lg:px-0'>
        <Navbar></Navbar>
         <AnimatePresence>
           <motion.div
