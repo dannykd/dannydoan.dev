bc54e891abe5c9cff260be9b4ddc6d8db81587d4
Danny Doan <dannykdoan@gmail.com>
2022-09-29 02:56:26 -0700

    text size change

--- src/pages/components/SongBar.tsx
+++ src/pages/components/SongBar.tsx
@@ -22,9 +22,9 @@ const SongBar = () => {
         className="sticky top-0 z-50"
       >
 
-      <div className="text-sm flex justify-between md:text-md font-medium text-left border-b border-neutral-300 text-neutral-300 bg-[#0F0E0E]">   
+      <div className="text-xs md:text-sm flex justify-between md:text-md font-medium text-left border-b border-neutral-300 text-neutral-300 bg-[#0F0E0E]">   
 
-          <p className="p-4 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
+          <p className="p-2 md:p-4 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
               Not Playing Anything <span><SiSpotify className="h-4 w-4 md:h-5 md:w-5 flex-shrink-0" /></span>
           </p>
 
@@ -48,12 +48,12 @@ const SongBar = () => {
     >
 
     
-    <div className="text-sm flex justify-between md:text-md font-medium text-left border-b border-neutral-400 text-neutral-400 bg-[#0F0E0E]">   
+    <div className="text-xs md:text-sm flex justify-between text-left border-b border-neutral-300 text-neutral-300 bg-[#0F0E0E]">   
       <a target="_blank"
         rel="noreferrer"
         href={`https://open.spotify.com/track/${data.spotify.track_id}`}
       >
-        <p className="p-4 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
+        <p className="p-2 md:p-4 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
             Currently Listening To: <span className="underline">{data?.spotify?.song}</span> by {data?.spotify?.artist}<span>
               <SiSpotify className="h-4 w-4 md:h-5 md:w-5 flex-shrink-0 text-green-400" /></span>
         </p>
