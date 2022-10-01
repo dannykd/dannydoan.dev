58ad759f3ed15d08230151f53e60a06b24e93738
Danny Doan <dannykdoan@gmail.com>
2022-09-30 22:24:30 -0700

    padding again...

--- src/pages/components/SongBar.tsx
+++ src/pages/components/SongBar.tsx
@@ -33,7 +33,7 @@ const SongBar = () => {
 
       <div className="mx-auto max-w-4xl text-xs md:text-sm flex justify-between font-medium text-left border-b
        border-neutral-300 dark:text-neutral-300 bg-neutral-100 dark:bg-[#0F0E0E]">   
-          <p className="py-3 md:px-4 px-2 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
+          <p className="py-3 px-2 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
               Not Playing Anything <span><SiSpotify className="h-4 w-4 md:h-5 md:w-5 flex-shrink-0" /></span>
           </p>
 
