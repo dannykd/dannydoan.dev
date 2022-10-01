04a8db40e254971b0f9179b934f38e208baa32cf
Danny Doan <dannykdoan@gmail.com>
2022-09-30 21:51:09 -0700

    experience card color change

--- src/pages/components/Experience.tsx
+++ src/pages/components/Experience.tsx
@@ -11,11 +11,11 @@ interface ExperienceProps{
 const Experience = ({ img, role, timespan, description }: ExperienceProps) => {
 
     return (
-        <div className="dark:bg-black bg-white ring-1 ring-black dark:ring-white hover:ring-blue-400 rounded-lg hover:outline-blue-400 px-2 p-4 text-base">
+        <div className="dark:bg-neutral-900 bg-white ring-1 ring-black dark:ring-white dark:hover:ring-blue-400 hover:ring-blue-400 rounded-lg hover:outline-blue-400 px-2 p-4 text-base">
             <div className="flex space-x-4 border-b border-black dark:border-white px-2">
                 <Image className="mb-2 rounded-lg" src={img} height="48" width="48" layout="fixed"></Image>
                 <div className="flex flex-col mb-2 space-y-2">
-                    <h1 className="text-blue-white">{role}</h1>
+                    <h1 className="">{role}</h1>
                     <p className="text-sm">{timespan}</p>
                 </div>
             </div>
