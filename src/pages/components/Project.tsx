dc510db5b6e1a8386c0aaa1229775c77b779d608
Danny Doan <dannykdoan@gmail.com>
2022-10-09 01:52:23 -0700

    padding

--- src/pages/components/Project.tsx
+++ src/pages/components/Project.tsx
@@ -40,7 +40,7 @@ const Project = ({ title, description, gh_url, proj_url }: Props) => {
     >
       <button
         onClick={toggle}
-        className="z-10 flex select-none items-center text-lg p-3"
+        className="z-10 flex select-none items-center text-lg p-3 px-4"
       >
         <div className="flex flex-1 dark:text-white">
           <p>{title}</p>
