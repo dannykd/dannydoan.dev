f708cc983bd4f64f0d3357ae6e339d067215fe75
Danny Doan <dannykdoan@gmail.com>
2022-11-06 04:10:01 -0800

    darker

--- src/pages/components/Experience.tsx
+++ src/pages/components/Experience.tsx
@@ -11,8 +11,8 @@ interface ExperienceProps{
 const Experience = ({ img, role, timespan, description }: ExperienceProps) => {
 
     return (
-        <div className="dark:bg-neutral-900 bg-white rounded-lg border dark:border-white/20 border-black/20 px-2 p-4">
-            <div className="flex space-x-4 border-b border-black/20 dark:border-white/20 px-2">
+        <div className="dark:bg-[#0a0a0a] bg-white rounded-lg border dark:border-white/30 border-black/20 px-2 p-4">
+            <div className="flex space-x-4 border-b border-black/20 dark:border-white/30 px-2">
                 <Image className="mb-2 rounded-lg" src={img} height="48" width="48" layout="fixed"></Image>
                 <div className="flex flex-col mb-2 space-y-2">
                     <h1 className="">{role}</h1>
--- src/pages/components/Project.tsx
+++ src/pages/components/Project.tsx
@@ -28,8 +28,8 @@ const Project = ({ title, description, gh_url, proj_url }: Props) => {
   return (
     <motion.div
       animate={{ height: open ? "auto" : "56px" }}
-      className="card relative flex flex-col overflow-hidden rounded-md border dark:border-white/20
-      bg-white dark:bg-neutral-900 border-black/20"
+      className="card relative flex flex-col overflow-hidden rounded-md border dark:border-white/30
+      bg-white dark:bg-[#0a0a0a] border-black/20"
       style={
         {
           "--cursor-x": coords.x + "px",
@@ -55,7 +55,7 @@ const Project = ({ title, description, gh_url, proj_url }: Props) => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="z-10 mx-4 flex h-full border-t dark:border-white/20 border-black/20"
+            className="z-10 mx-4 flex h-full border-t dark:border-white/30 border-black/20"
           >
             <div className="flex flex-col space-y-4 px-2 py-4">
               <p className="flex-1 select-none text-neutral-700 dark:text-neutral-300">{description}</p>
