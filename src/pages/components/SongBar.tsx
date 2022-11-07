ccb0b266544a92d4355ea72727553070860aa24b
Danny Doan <dannykdoan@gmail.com>
2022-11-07 00:01:56 -0800

    fixed border

--- src/pages/components/SongBar.tsx
+++ src/pages/components/SongBar.tsx
@@ -28,12 +28,12 @@ const SongBar = () => {
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className="sticky top-0 z-50 bg-inherit"
+        className="sticky top-0 z-50 bg-inherit border-b border-neutral-300 max-w-4xl mx-auto"
       >
 
-      <div className="mx-auto max-w-4xl text-xs md:text-sm flex justify-between font-medium text-left border-b
+      <div className="text-xs md:text-sm flex justify-between font-medium text-left 
        border-neutral-300 dark:text-neutral-300">   
-          <p className="py-3 px-2 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
+          <p className="py-3 px-2 rounded-t-lg focus:text-gray-600 focus:border-gray-300">
               Not Playing Anything <span><SiSpotify className="h-4 w-4 md:h-5 md:w-5 flex-shrink-0" /></span>
           </p>
 
@@ -73,16 +73,16 @@ const SongBar = () => {
     initial={{ y: -10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.5 }}
-    className="sticky top-0 z-50 bg-inherit"
+    className="sticky top-0 z-50 bg-inherit border-b border-neutral-300 max-w-4xl mx-auto"
     >
 
-    <div className="mx-auto max-w-4xl text-xs md:text-sm flex justify-between text-left border-b border-neutral-300
+    <div className="text-xs md:text-sm flex justify-between text-left
        dark:text-neutral-300">
       <a target="_blank"
         rel="noreferrer"
         href={`https://open.spotify.com/track/${data.spotify.track_id}`}
       >
-        <p className="py-3 px-2 rounded-t-lg border-b-2 border-transparent focus:text-gray-600 focus:border-gray-300">
+        <p className="py-3 px-2 rounded-t-lg focus:text-gray-600 focus:border-gray-300">
             Currently Listening To: <span className="underline">{data?.spotify?.song}</span> by {data?.spotify?.artist}<span>
               <SiSpotify className="h-4 w-4 md:h-5 md:w-5 flex-shrink-0 text-green-400" /></span>
         </p>
@@ -94,24 +94,7 @@ const SongBar = () => {
             ? <SunIcon className="h-12 w-12 bg-[#0F0E0E] border border-white text-white p-2 rounded-xl"/>
             : <MoonIcon className="h-12 w-12 bg-neutral-100 border border-black text-black p-2 rounded-xl"/>
           }
-      </button>
-      {/* <button>
-        <Menu as="div" className="relative inline-block text-left">
-            <Menu.Button className="inline-flex rounded-md py-2 text-sm font-medium text-white hover:bg-opacity-30
-              focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                <Bars3Icon className="h-11 w-11 outline outline-1 dark:outline-white dark:text-white text-black outline-black p-2 rounded-xl"/>
-            </Menu.Button>
-        
-            <Menu.Items className="absolute right-0 mt-2 w-56 md:w-72 origin-top-right divide-y divide-gray-100 rounded-md 
-            shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-white">
-                <div className="px-1 py-1 ">
-                    <MenuLink pageName="/experience" link="../index" ></MenuLink>
-                    <MenuLink pageName="/contact" link="../index" ></MenuLink>
-                </div>
-            </Menu.Items>
-        </Menu>
-      </button> */}
-      
+      </button>    
     </div>
 
     </motion.div>
