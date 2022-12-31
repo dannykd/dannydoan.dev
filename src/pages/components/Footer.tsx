5ff7deed70cd8cbfacfcce30d0fa062d425882f0
Danny Doan <dannykdoan@gmail.com>
2022-12-31 00:44:22 -0800

    source code link in footer

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -9,10 +9,12 @@ const Footer: FC<FooterProps> = () => {
     initial={{ y: -10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.5 }}
-    className="flex justify-center bg-neutral-100 p-4 text-xs text-black transition-colors dark:bg-[#060606]
+    className="flex max-w-4xl mx-auto justify-between bg-neutral-100 p-4 text-xs text-black transition-colors dark:bg-[#060606]
      dark:text-white md:text-sm bottom-0 left-0 right-0 absolute text-neutral-500 dark:text-neutral-500"
     >
-        <p>{"/* built with ♥ by danny */"}</p>
+
+        <p>{"built with ♥ by danny"}</p>
+        <a href="https://github.com/dannykd/dannydoan.dev">source↗</a>
     </motion.div>
 
 
