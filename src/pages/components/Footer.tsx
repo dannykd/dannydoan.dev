4c7eb4647cc18a3b4ff889b3dbd2d9650320ac7f
Danny Doan <dannykdoan@gmail.com>
2022-12-31 00:25:02 -0800

    lower contrast text-color on footer; added commenting syntax for styling

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -9,10 +9,10 @@ const Footer: FC<FooterProps> = () => {
     initial={{ y: -10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.5 }}
-    className="flex justify-center bg-neutral-100 p-4 text-xs text-black
-    transition-colors dark:bg-[#060606] dark:text-white md:text-sm bottom-0 left-0 right-0 absolute"
+    className="flex justify-center bg-neutral-100 p-4 text-xs text-black transition-colors dark:bg-[#060606]
+     dark:text-white md:text-sm bottom-0 left-0 right-0 absolute text-neutral-500 dark:text-neutral-500"
     >
-        <p>built with ♥ by danny</p>
+        <p>{"/* built with ♥ by danny */"}</p>
     </motion.div>
 
 
